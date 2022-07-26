ea03c87a44ef5c23bcfdb9bab8d2054e1bacb332
Exodia <67595890+DonRP@users.noreply.github.com>
2022-07-26 22:41:44 +0200

    clear

--- bin/renpy.ps1
+++
@@ -1,8 +0,0 @@
-param(
-    [string] $Command = "run"
-)
-$RenPy = Get-Content (Resolve-Path ".renpy-sdk")
-$RenPy = [string]::join("", ($RenPy.Split("`n")))
-
-Write-Output "Using Ren'Py SDK: $RenPy"
-Start-Process -FilePath "$RenPy\renpy.exe" -ArgumentList ". $Command"
